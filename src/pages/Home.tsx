70b7a34e69c5f7fbcb977f57bf7331827e094606
chasura416 <chasura416@naver.com>
2024-02-05 16:16:41 +0900

    #7 add logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ import {
 
 import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-
+import logo from "../assets/pslogo.png"
 
 const Home = () => {
   const [ search, setSearch ] = useState("");
@@ -42,8 +42,10 @@ const Home = () => {
 
   return (
     <div className="w-full flex flex-col">
-      <div className="flex justify-between">
-        <div>로고</div>
+      <div className="flex justify-between m-5">
+        <div>
+          <img src="/src/assets/pslogo.png" alt="logo" width={64} height={1}/>
+        </div>
         {/* <div className="flex">
             <input 
               className="border"
@@ -54,7 +56,7 @@ const Home = () => {
             />
           <IoSearchSharp />
         </div> */}
-        <form>   
+        <form className="mt-2">   
           <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -63,22 +65,22 @@ const Home = () => {
                   </svg>
               </div>
               <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
-              <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+              <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">검색</button>
           </div>    
         </form>
-        <div className="flex gap-3">
-          <div>로그인</div>
-          <div>마이페이지</div>
-        </div>
+        <ul className="flex space-x-5 mt-4">
+          <li>로그인</li>
+          <li>마이페이지</li>
+        </ul>
       </div>
-      <div className="flex">
+      <div className="flex justify-around">
         <div className="w-100 border-b">사이드 카테고리</div>
-        <div className="w-700 border-b">
+        <div className="w-100">
           <Carousel>
             <CarouselContent>
-              <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem>
-              <CarouselItem>...</CarouselItem>
+              <CarouselItem>...1</CarouselItem>
+              <CarouselItem>...2</CarouselItem>
+              <CarouselItem>...3</CarouselItem>
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
