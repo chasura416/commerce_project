439480d9b70bbaa4cc540b3188c6574daa4947cf
chasura416 <chasura416@naver.com>
2024-02-10 19:04:22 +0900

    #4 image upload 기능 리팩토링

--- src/hooks/upload/useFileUpload.ts
+++ src/hooks/upload/useFileUpload.ts
@@ -1,21 +1,74 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-import { addDoc, collection, getDocs, query, Timestamp } from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore, orderBy, query, Timestamp } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes, deleteObject } from "firebase/storage";
 
 import { db, storage, auth } from "@/firebase";
 
 const useFileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [text, setText] = useState("");
-
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [price, setPrice] = useState("");
+  
   const [products, setProducts] = useState([]);
+  
+  const fileInput = useRef(null);
+  const [imageUpload, setImageUpload] = useState("");
+  const [image, setImage] = useState("");
+  const [images, setImages] = useState([]);
+  const [uploadStep, setUploadStep] = useState(1);
+
+
+  const fetchImages = async () => {
+    const photo = collection(getFirestore(firebaseApp), "photo");
+    const result  = await getDocs(query(photo, orderBy("timestamp", desc)));
+    const fetchData = result.docs.map((el) => el.data());
+    setImages(fetchData);
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
+
+  const UploadImgUrl = async () => {
+    await addDoc(collection(db, `${auth.currentUser?.uid}`), {
+      imgUrl: image,
+      timestamp: new Date(),
+    });
+    fetchImages();
+    setImageUpload("");
+    setImage("");
+    setUploadStep(1);
+  }
+
+
+  const selectFile = (file) => {
+    console.log(file);
+    setImageUpload(file);
+    setUploadStep(2);
+  }
+
+
+  useEffect(()=>{
+    const imageRef = ref(storage, `${auth.currentUser?.uid}/${imageUpload.name}`)
+    if (!imageUpload) return;
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImage(url);
+        console.log(url);
+      })
+    })
+  }, [imageUpload]);
+
+
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleUpload = async () => {
+  const handleUpload = async (event) => {
     // ref 함수를 이용해서 Storage 내부 저장할 위치를 지정하고, uploadBytes 함수를 이용해서 파일을 저장합니다.
     const imageRef = ref(storage, `${auth.currentUser?.uid}/${selectedFile.name}`);
     await uploadBytes(imageRef, selectedFile);
@@ -23,6 +76,25 @@ const useFileUpload = () => {
     // 파일 URL 가져오기
     const downloadURL = await getDownloadURL(imageRef);
     console.log(downloadURL);
+
+    //내용 업로드 되야함 
+    event.preventDefault();
+    const newProducts = { 
+      title: String,
+      createdAt: Timestamp,
+      price: Number, 
+    };
+
+    const collectionRef = collection(db, "Products");
+    console.log(collectionRef)
+    const { id } = await addDoc(collectionRef, newProducts);
+
+    setProducts((prev) => {
+      return [...products, { ...newProducts, id }];
+    });
+    setTitle("");
+    setPrice("");
+    setContent("");
   };
 
   const deleteImage = async () => {
@@ -52,8 +124,14 @@ const useFileUpload = () => {
     const {
       target: { name, value },
     } = event;
-    if (name === "text") {
-      setText(value);
+    if (name === "title") {
+      setTitle(value);
+    }
+    if (name === "price") {
+      setPrice(value);
+    }
+    if (name === "content") {
+      setContent(value);
     }
   };
   
@@ -79,10 +157,9 @@ const useFileUpload = () => {
   const addProduct = async (event) => {
     event.preventDefault();
     const newProducts = { 
-      title: text,
+      title: String,
       createdAt: Timestamp,
       price: Number, 
-      like: false 
     };
 
     const collectionRef = collection(db, "Products");
@@ -95,7 +172,7 @@ const useFileUpload = () => {
     setText("");
   };
 
-  return {selectedFile, text, products, handleFileSelect, handleUpload, deleteImage, selectImg, onChange, addProduct }
+  return {selectedFile, products, title, price, image, content, fileInput, imageUpload ,images, uploadStep, handleFileSelect, handleUpload, deleteImage, selectImg, onChange, addProduct }
 
 };
 
--- src/pages/ProductUpload.tsx
+++ src/pages/ProductUpload.tsx
@@ -5,8 +5,15 @@ import { Button } from "@/components/ui/button";
 const ProductUpload = () => {
   const {
     selectedFile,
-    text,
     products,
+    title,
+    price,
+    image,
+    content,
+    fileInput,
+    imageUpload,
+    images,
+    uploadStep,
     handleFileSelect,
     handleUpload,
     deleteImage,
@@ -21,21 +28,41 @@ const ProductUpload = () => {
       <h2>판매 글 작성</h2>
       <form className="p-10">
         <div className="flex flex-col border p-10 h-full">
-          <img src={selectedFile} />
+          <img src={selectedFile} alt="응 안떠"/>
           <input className="border" type="file" onChange={handleFileSelect} />
           <label>제목 : </label>
           <input
             className="border"
             type="text"
-            value={text}
-            name="text"
+            value={title}
+            name="title"
+            onChange={onChange}
+            required
+          ></input>
+          <label>가격 : </label>
+          <input
+            className="border"
+            type="text"
+            value={price}
+            name="price"
             onChange={onChange}
             required
           ></input>
           <label>내용 : </label>
-          <textarea className="border min-h-52" />
+          <input
+            className="border"
+            type="textarea"
+            value={content}
+            name="content"
+            onChange={onChange}
+            required
+          ></input>
+          <label>내용 : </label>
+          <textarea 
+            className="border min-h-52" 
+          />
           <Button 
-            onClick={addProduct}>
+            onClick={handleUpload}>
               작성하기
           </Button>
         </div>
