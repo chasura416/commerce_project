d7ad70c5e62a9e1c5f38cd10760fa3ee23081903
chasura416 <chasura416@naver.com>
2024-03-10 23:55:41 +0900

    add react Error Boundary lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Router from "./router/Router";
 import AuthProvider from "./context/auth/AuthContext";
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+
+const ErrorFallbackRender = ({error}: FallbackProps) => {
+  return (
+    <p className="mt-6 text-base leading-7 text-gray-600">{error.message}</p>  
+  )
+}
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Router />
-      </AuthProvider>
+      <ErrorBoundary fallbackRender={ErrorFallbackRender}>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </ErrorBoundary>
     </>
   );
 }
