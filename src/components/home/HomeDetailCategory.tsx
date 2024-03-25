052cec6f4b8a153084b29535149a87abf2d166a9
chasura416 <chasura416@naver.com>
2024-03-25 21:18:43 +0900

    #32 home category detail form 추가

---
+++ src/components/home/HomeDetailCategory.tsx
@@ -0,0 +1,19 @@
+import { FLATFORM } from "../Catogory/categoryFlatform";
+
+
+const HomeDetailCategory = () => {
+  
+  return (
+    <>
+    <section className="flex">
+      <div className="flex justify-between">
+        <div className="border-b text-2xl">HomeDetailCategory</div>
+        <div>더 보기</div>
+      </div>
+      <div>상품 컴포넌트 들어갈 곳</div>
+    </section>
+    </>
+  )
+}
+
+export default HomeDetailCategory;
