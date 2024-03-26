63f3b3084cf7099183d26c45324b9cb2e7a78d88
chasura416 <chasura416@naver.com>
2024-03-26 20:23:52 +0900

    #32 home category 추가

--- src/components/Catogory/categoryFlatform.ts
+++ src/components/Catogory/categoryFlatform.ts
@@ -4,4 +4,12 @@ export const FLATFORM: Record<string, {title: string}> = {
   ps4: { title: 'PlayStation4' },
   xbox: { title: 'XBOX ONE' },
   switch: { title: 'NINTENDO SWITCH' },
-};
+}
+;
+export const FLATFORMList = [
+  { title: 'Category All' },
+  { title: 'PlayStation5' },
+  { title: 'PlayStation4' },
+  { title: 'XBOX ONE' },
+  { title: 'NINTENDO SWITCH' },
+];
--- src/components/home/HomeDetailCategory.tsx
+++ src/components/home/HomeDetailCategory.tsx
@@ -1,19 +1,20 @@
-import { FLATFORM } from "../Catogory/categoryFlatform";
-
+import { FLATFORMList } from "../Catogory/categoryFlatform";
+import ProductsListCard from "../products/ProductListCard";
 
 const HomeDetailCategory = () => {
-  
   return (
     <>
-    <section className="flex">
-      <div className="flex justify-between">
-        <div className="border-b text-2xl">HomeDetailCategory</div>
-        <div>더 보기</div>
-      </div>
-      <div>상품 컴포넌트 들어갈 곳</div>
-    </section>
+      {FLATFORMList.map(({title}) => (
+        <section key={title} className="flex flex-col w-full">
+          <div className="flex justify-between">
+            <div className="border-b text-2xl">{title}</div>
+            <div>더 보기</div>
+          </div>
+          <ProductsListCard />
+        </section>
+      ))}
     </>
-  )
-}
+  );
+};
 
 export default HomeDetailCategory;
