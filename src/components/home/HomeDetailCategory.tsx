1c1409be89aeb5629dbdd19692a113cb1347b109
chasura416 <chasura416@naver.com>
2024-03-27 18:59:32 +0900

    #32 카테고리 더보기 링크 추가

--- src/components/home/HomeDetailCategory.tsx
+++ src/components/home/HomeDetailCategory.tsx
@@ -1,14 +1,17 @@
 import { FLATFORMList } from "../Catogory/categoryFlatform";
 import ProductsListCard from "../products/ProductListCard";
+import { Link } from "react-router-dom";
 
 const HomeDetailCategory = () => {
   return (
     <>
-      {FLATFORMList.map(({title}) => (
+      {FLATFORMList.map(({ title }) => (
         <section key={title} className="flex flex-col w-full">
           <div className="flex justify-between">
             <div className="border-b text-2xl">{title}</div>
-            <div>더 보기</div>
+            <Link to="{/category}">
+              <div className="cursor-pointer">더 보기</div>
+            </Link>
           </div>
           <ProductsListCard />
         </section>
