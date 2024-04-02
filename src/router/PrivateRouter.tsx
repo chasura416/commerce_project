600f0aba2ab6b315744cf938d16a5d5ec751beaa
chasura416 <chasura416@naver.com>
2024-04-02 22:03:59 +0900

    #1 private route 추가

--- src/router/PrivateRouter.tsx
+++ src/router/PrivateRouter.tsx
@@ -1,27 +1,14 @@
-import { ReactNode, useContext } from "react";
-import { Link } from "react-router-dom";
 import { Outlet, Navigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/firebase";
-import { AuthContext } from "@/context/auth/AuthContext";
 
-interface Props {
-  children: ReactNode;
-}
+// interface PrivateRouteProps {
+//   path: string;
+//   element: React.ReactElement;
+//   // children: ReactNode;
+// }
 
+const PrivateRouter = () => {
+  return auth.currentUser ? <Outlet /> : <Navigate to="/login" />;
+};
 
-const PrivateRouter = ({children}: Props) => {
-  const { user, loading } = useContext(AuthContext); 
-  
-  if(loading) {
-    return <span className="loading loading-dots"></span>
-  }
-
-  if(user) {
-    return children;
-  }
-
-  return <Link to="/login">{children}</Link>
-}
-
-export default PrivateRouter
+export default PrivateRouter;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Login, SignUpPage, MyPage, Cart, ProductUpload, NotFound, SellerPage, ProductDetail, CategoryList, ProductEdit, } from "../pages";
+import {
+  Home,
+  Login,
+  SignUpPage,
+  MyPage,
+  Cart,
+  ProductUpload,
+  NotFound,
+  SellerPage,
+  ProductDetail,
+  CategoryList,
+  ProductEdit,
+} from "../pages";
 import PrivateRouter from "./PrivateRouter";
 
 const Router = () => {
@@ -8,12 +20,6 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/mypage" element={
-          <PrivateRouter>
-            <MyPage />
-          </PrivateRouter>
-        } 
-      />
       <Route path="/upload" element={<ProductUpload />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/productedit/:id" element={<ProductEdit />} />
@@ -21,6 +27,11 @@ const Router = () => {
       <Route path="/productdetail/:id" element={<ProductDetail />} />
       <Route path="/category/:id" element={<CategoryList />} />
       <Route path="/*" element={<NotFound />} />
+
+      {/* Private Route */}
+      <Route element={<PrivateRouter/>}>
+        <Route path="/mypage" element={<MyPage />} />
+      </Route>
     </Routes>
   );
 };
