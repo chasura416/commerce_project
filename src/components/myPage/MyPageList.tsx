86db9f564a7279b46d97d6651e18a050e522c18e
chasura416 <chasura416@naver.com>
2024-04-12 22:46:42 +0900

    #7 mypage list ui 개선작업

--- src/components/myPage/MyPageList.tsx
+++ src/components/myPage/MyPageList.tsx
@@ -2,7 +2,11 @@
 
 const MyPageList = () => {
   return (
-    <aside className="w-20 h-96 bg-red-200"></aside>
+    <aside className="w-36 h-96 bg-red-200">
+      <div>내가 쓴 글 목록</div>
+      <div>내가 좋아요 한 글 목록</div>
+      <div></div>
+    </aside>
   )
 }
 
