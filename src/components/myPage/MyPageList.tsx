1760eb973abd5c15f16a93197e4e9aa11f06e0a8
chasura416 <chasura416@naver.com>
2024-04-17 00:14:40 +0900

    #7 MyPage ui 변경

--- src/components/myPage/MyPageList.tsx
+++ src/components/myPage/MyPageList.tsx
@@ -2,10 +2,11 @@
 
 const MyPageList = () => {
   return (
-    <aside className="w-36 h-96 bg-red-200">
-      <ul>
-        <ol>내가 쓴 글 목록</ol>
-        <ol>내가 좋아요 한 글 목록</ol>
+    <aside className="w-36 h-screen bg-blue-600 p-3 pt-5">
+      <ul className="flex flex-col gap-5 font-semibold text-white">
+        <li className="cursor-pointer">내가 쓴 글</li>
+        <li className="cursor-pointer">내가 좋아요 한 글</li>
+        <li>asdf</li>
       </ul>
       <div></div>
       <div></div>
