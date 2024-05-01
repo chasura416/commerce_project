caff785385fe9106a62055c9ec65d4c7dfc8618c
chasura416 <chasura416@naver.com>
2024-05-01 23:20:27 +0900

    #7 MypageList 수정

--- src/components/myPage/MyPageList.tsx
+++ src/components/myPage/MyPageList.tsx
@@ -1,11 +1,15 @@
-
+import { Link } from "react-router-dom"
 
 const MyPageList = () => {
   return (
     <aside className="w-36 h-screen bg-blue-600 p-3 pt-5">
-      <ul className="flex flex-col gap-5 font-semibold text-white">
-        <li className="cursor-pointer">내가 쓴 글</li>
-        <li className="cursor-pointer">내가 좋아요 한 글</li>
+      <ul className="cursor-pointer flex flex-col gap-5 font-semibold text-white">
+        <Link to={``}>
+          <li className="border-b hover:text-gray-400">내가 쓴 글</li>
+        </Link>
+        <Link to={``}>
+          <li className="border-b hover:text-gray-400">내가 좋아요 한 글</li>
+        </Link>
         <li>asdf</li>
       </ul>
       <div></div>
